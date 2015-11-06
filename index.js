156dea81819b490fd22da9994808a2a4f4365559
siddharth <gyrogrid@gmail.com>
2015-11-06 23:06:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
