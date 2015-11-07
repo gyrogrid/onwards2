603126ed435b2f2d0576a611b3892fb81ce3a03d
siddharth <gyrogrid@gmail.com>
2015-11-07 14:13:46 +0530

    jquery source to https

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static('public'));
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-
-app.use(express.static('public'));
 
 app.get('/', function(req, res){
 	res.render('index');
