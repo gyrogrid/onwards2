f44f9229fdd674c7d15e14541246a7f054ecd358
siddharth <gyrogrid@gmail.com>
2015-11-07 14:04:14 +0530

    modified index.js 2

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var exphbs = require('express-handlebars');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
@@ -10,6 +12,10 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res){
 	res.render('index');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('node app is on port', app.get('port'));
 });
 
 /*var server = app.listen(3000, function () {
