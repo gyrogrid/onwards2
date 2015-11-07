85fce43a6396dd8e936843b8e261f7e577e0cf67
siddharth <gyrogrid@gmail.com>
2015-11-07 13:57:11 +0530

    modified index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.get('/', function(req, res){
 	res.render('index');
 });
 
-var server = app.listen(3000, function () {
+/*var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-});
+});*/
