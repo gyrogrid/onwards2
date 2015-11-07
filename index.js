73b710536d0d7de5956842540446c22aa2bdc85f
siddharth <gyrogrid@gmail.com>
2015-11-07 15:11:29 +0530

    removed redundant js files

--- index.js
+++ index.js
@@ -17,10 +17,3 @@ app.get('/', function(req, res){
 app.listen(app.get('port'), function(){
 	console.log('node app is on port', app.get('port'));
 });
-
-/*var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});*/
